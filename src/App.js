5c570b33d7be35a759bd3e963846a511b282133b
LuisKevL <20213tn118@utez.edu.mx>
2023-01-31 12:32:44 -0600

    uso de hooks

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import HolaMundo, {HolaPais} from "./components/HolaMundo";
 import Saludar from "./components/Saludar";
+import {useState, useEffect} from "react";
+
 function App() {
   const name = "Emiliano Ortiz"
   const objeto = {
@@ -14,18 +16,35 @@ function App() {
   const funApp=()=>{
     alert("hola desde app")
   }
+
+
+  const [estadoAuto, seteStadoAuto]= useState(false)
+  const turnOff=()=>{
+    //seteStadoAuto(!estadoAuto)
+    seteStadoAuto(preValue=>!preValue)
+    setConteo(conteo+1)
+  }
+  const [conteo, setConteo] = useState(0)
+
+  useEffect(()=>{
+    console.log("el conteo cambio "+conteo)
+  },[conteo])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/*<p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <Saludar texto="mi primer prop"
           name = {name}
           usuario = {objeto}
                  funcion = {funApp}
-        />
+        />*/}
+        <h3>El automovil esta: {estadoAuto?"encendido":"apagado"}</h3>
+        <h3>Has dado {conteo} clicks</h3>
+        <button onClick={turnOff}>Encender/Apagar</button>
       </header>
     </div>
   );
